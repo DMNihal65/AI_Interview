7fb13b51a538d1dc78276da5541666b4a16902ba
DMNihal65 <Nihaldm65@gmail.com>
2024-07-06 23:44:10 +0530

    added the header/navbar to the main application and proteced the route using the clerk auth

--- ai_mock_interview/app/dashboard/layout.jsx
+++ ai_mock_interview/app/dashboard/layout.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Header from './_components/Header'
 function DashboardLayout({children}) {
   return (
     <div>
+        <Header/>
         {children}
     </div>
   )
--- ai_mock_interview/app/dashboard/page.jsx
+++ ai_mock_interview/app/dashboard/page.jsx
@@ -5,7 +5,6 @@ function Dashboard() {
   return (
     <div>
         Dashboard
-        <UserButton/>
     </div>
   )
 }
